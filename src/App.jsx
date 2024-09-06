eb83eb676205785f7410f703c5c3d164a4f78b88
np03s190038 <thapa.sumit007@gmail.com>
2024-09-06 12:23:14 +0545

    adding props to a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Card from "./components/Card.Jsx"
+import cardImage from "./assets/katie-zaferes.png"
+import weddingImage from "./assets/wedding-photography.png"
+import mountainBike from "./assets/mountain-bike.png"
 
 function App(){
   return(
     <>
       <Header />
       <Hero />
-      <Card />
+      <Card img={cardImage} rating={5.0} reviewCount={6} country="Nepal" title="Life Lessons with Katie Zaferes" price={136} />
+      <Card img={weddingImage} rating={4.0} reviewCount={5} country="India" title="Blissful wedding in the Himalayas" price={236} />
+      <Card img={mountainBike} rating={4.5} reviewCount={4} country="Switzerland" title="Explore the rocky mountains" price={186} />
     </>
   )
 }
 
 export default App
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
