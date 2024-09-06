ed4e29cb7f604ac133278b48d6a40397e0f658e2
np03s190038 <thapa.sumit007@gmail.com>
2024-09-06 06:54:35 +0545

    adding assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Card from "./components/Card.Jsx"
 
 function App(){
   return(
     <>
       <Header />
       <Hero />
+      <Card />
     </>
   )
 }
